cef4266c2e57b0d198abba0cb59351b24e28d78f
Archimondee <kikchiemedia@gmail.com>
2025-08-01 22:29:06 +0700

    Add support vercel

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "@react-router/dev/config";
+import { vercelPreset } from "@vercel/react-router/vite";
 
 export default {
 	// Config options...
@@ -6,4 +7,5 @@ export default {
 	ssr: true,
 	routeDiscovery: { mode: "lazy" },
 	serverBuildFile: "build/index.js",
+	presets: [vercelPreset()],
 } satisfies Config;
