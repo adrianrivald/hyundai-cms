80d7b0d1a04bf18a24f9fe19810e0b6e273551de
Archimondee <kikchiemedia@gmail.com>
2025-08-31 23:52:10 +0700

    Add social media edit

--- app/page/content-editor/social-media/_functions/hooks/use-list-social-media.ts
+++ app/page/content-editor/social-media/_functions/hooks/use-list-social-media.ts
@@ -1,19 +1,24 @@
-import { useTodosList } from "@/api/todos";
 import { useTableConfig } from "@/hooks/use-table-config";
 import { useTableState } from "@/hooks/use-table-state";
-import type { AlbumTypes } from "@/types/PostTypes";
 import { dataSocmedColumn } from "../columns/social-media-column";
+import { useGetGlobalVariables } from "@/api/global-variable";
+import type { GlobalVariableTypes } from "@/types/GlobalVariableTypes";
 
 export function useListSocialMedia() {
 	const tableState = useTableState({});
 
-	const { data, refetch } = useTodosList({
-		queryKey: ["socmed-list"],
+	const { data, refetch } = useGetGlobalVariables({
+		queryKey: ["global-variable-social-media"],
 		staleTime: 5 * 60 * 1000,
 	});
 
 	const table = useTableConfig({
-		data: (data?.slice(0, 10) as [] as AlbumTypes[]) ?? [],
+		data:
+			(
+				data?.data
+					.filter((item) => item.name === "social_media")
+					.filter((item) => !!item.var_value) as GlobalVariableTypes[]
+			)?.map((item) => JSON.parse(item.var_value))?.[0] ?? [],
 		columns: dataSocmedColumn,
 		tableState,
 		// pageCount: data?.meta?.totalPages ?? -1,
